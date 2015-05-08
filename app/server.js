025402e21ae80018d4fe38522d94ca4c3eae9f2f
Hans Eilers <hanseilers@Macintosh.local>
2015-05-08 22:56:05 +0200

    day 1

---
+++ app/server.js
@@ -0,0 +1,24 @@
+// Get the packages we need
+var express = require('express');
+
+// Create our Express application
+var app = express();
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+app.get('/', function(req, res) {
+  res.json({ message: 'This our property management API' });
+});
+
+// Register all our routes with /api
+app.use('/api/v1', app.router);
+
+// Start the server
+app.listen(port);
+console.log('Property management API started on ' + port);
--- app/test/test.js
+++ app/test/test.js
@@ -1,9 +1,33 @@
 var assert = require("assert");
+var should = require('should');
+var request = require('supertest');
 
-describe('Array', function(){
-  describe('indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Routing', function() {
+  var url = 'http://someurl.com';
+  before(function(done) { 
+	done();
+  });
+  
+  // use describe to give a title to your test suite, in this case the tile is "Account"
+  // and then specify a function in which we are going to declare all the tests
+  // we want to run. Each test starts with the function it() and as a first argument 
+  // we have to provide a meaningful title for it, whereas as the second argument we
+  // specify a function that takes a single parameter, "done", that we will use 
+  // to specify when our test is completed, and that's what makes easy
+  // to perform async test!
+  describe('Base', function() {
+    it('should return the title of the API', function(done) {
+    request(url)
+	.post('/api/v1')
+    // end handles the response
+	.end(function(err, res) {
+          if (err) {
+            throw err;
+          }
+          // this is should.js syntax, very clear
+          res.should.have.status(200);
+          done();
+        });
     });
   });
 });
